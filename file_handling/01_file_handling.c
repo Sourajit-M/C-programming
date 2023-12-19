26bdefba0e103349169ee8967500ec31e5176251
Sourajit-M <140721643+Sourajit-M@users.noreply.github.com>
2023-12-19 12:26:10 +0000

    file handling creating a new txt file with the help of c

---
+++ file_handling/01_file_handling.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fileptr; // File pointer declaration
+
+    // Creating a file named "example.txt" in write mode
+    fileptr = fopen("01_test.txt", "w");
+
+    if (fileptr == NULL) {
+        printf("File could not be opened.\n");
+        return 1;
+    }
+
+    // Writing content into the file
+    fprintf(fileptr, "Hello, this is some content written into the file.");
+
+    // Closing the file
+    fclose(fileptr);
+
+    printf("File created, written, and closed successfully.\n");
+
+    return 0;
+}
