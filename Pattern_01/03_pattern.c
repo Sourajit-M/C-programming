87e0097ca63fcd75c20a8214d9ce039d60d17d64
Sourajit-M <140721643+Sourajit-M@users.noreply.github.com>
2023-12-22 06:50:16 +0000

    new pattern hollow diamond added

--- Pattern_01/03_pattern.c
+++ Pattern_01/03_pattern.c
@@ -12,20 +12,81 @@ int main(){
     int n;
     printf("Enter a number : ");
     scanf("%d", &n);
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=n; j++){
-            if(i==1 || i==5){
-                printf("* ");
+
+
+    // for(int i=1; i<=n; i++){
+    //     for(int j=1; j<=n; j++){
+    //         if(i==1 || i==5){
+    //             printf("* ");
+    //         }
+    //         else if(j==1 || j==5){
+    //             printf("* ");
+    //         }else{
+    //             printf("  ");
+    //         }
+    //     }
+
+    //     printf("\n");
+    // }
+
+
+
+    /*
+
+      **********
+      ****  ****
+      ***    ***
+      **      **
+      *        *
+      *        *
+      **      **
+      ***    ***
+      ****  ****
+      **********
+    
+    
+    */
+
+   for(int i=0; i<2*n; i++){
+    if(i < n){
+        for(int j=0; j<n-i; j++){
+            printf("*");
             }
-            else if(j==1 || j==5){
-                printf("* ");
-            }else{
-                printf("  ");
-            }
+
+        for(int k=0; k<i; k++){
+            printf(" ");
         }
 
-        printf("\n");
+        for(int m=0; m<i; m++){
+            printf(" ");
+        }
+
+        for(int p=0; p<n-i; p++){
+            printf("*");
+        }
+
+        
+    }else{
+        for(int j=0; j<i-n+1; j++){
+            printf("*");
+            }
+
+        for(int k=n; k>i-n+1; k--){
+            printf(" ");
+        }
+
+        for(int m=n; m>i-n+1; m--){
+            printf(" ");
+        }
+
+        for(int p=0; p<i-n+1; p++){
+            printf("*");
+        }
+
     }
+    printf("\n");
+    }
+     
 
 return 0;
 }
