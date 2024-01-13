b13b112027d257708e3d59f5b3c8b8dc9a61f52e
Sourajit-M <140721643+Sourajit-M@users.noreply.github.com>
2024-01-13 06:38:38 +0000

    palindrome using recursion

--- suggestionsBySir/armstrong.c
+++ suggestionsBySir/armstrong.c
@@ -1,29 +1,48 @@
 #include <stdio.h>
 
-#define max_size 100;
-
+void armstrong(int num, int n1, int sum);
+void palindrome(int num, int n1, int sum);
 
 int main(){
     int num;
     printf("Enter a number: \n");
     scanf("%d", &num); 
 
-    armstrong(num, num, 0);
+    palindrome(num, num, 0);
 
     return 0;
 }
 
 void armstrong(int num, int n1, int sum){
-    if(num==sum){
-        printf("%d is a Armstrong Number", num);
+    if(num == sum){
+        printf("%d is an Armstrong Number\n", num);
+        return;
     }
 
     if(n1 == 0){
-        printf("%d is not a Armstrong Number", num);
+        printf("%d is not an Armstrong Number\n", num);
+        return;
     } 
 
     else{
-        int d = n1 %10 ;
-        armstrong(num, num/10, d*d*d);
+        int d = n1 % 10;
+        armstrong(num, n1 / 10, sum + d * d * d);
+    }
+}
+
+void palindrome(int num, int n1, int rev){
+    if(num==rev){
+        printf("%d is a Palindrome Number\n", num);
+        return;
+    }
+
+    if(n1==0){
+        printf("%d is not Palindrome Number", num);
+        return;
+    }
+    else{
+        int d = n1%10;
+        rev = rev*10 + d;
+        palindrome(num, n1/10, rev);
     }
 }
