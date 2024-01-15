b6866ddf9b180b58829094ec9ca186af6fd92004
Sourajit-M <140721643+Sourajit-M@users.noreply.github.com>
2024-01-15 14:43:18 +0000

    merge two Sorted arrays

--- Pointers/pointers_04.c
+++ Pointers/pointers_04.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 struct Student {
     int roll_no;
@@ -9,25 +10,58 @@ struct Student {
 };
 
 int main() {
-    struct Student s1;
-    struct Student* ptr = &s1;
+    // struct Student s1;
+    // struct Student* ptr = &s1;
 
-    printf("Enter Name of Student\n");
-    fgets(ptr->name, sizeof(ptr->name), stdin);
+    // printf("Enter Name of Student\n");
+    // fgets(ptr->name, sizeof(ptr->name), stdin);
 
-    printf("Enter the Roll Number of Student\n");
-    scanf("%d", &ptr->roll_no);
+    // printf("Enter the Roll Number of Student\n");
+    // scanf("%d", &ptr->roll_no);
 
-    printf("Enter Branch of Student\n");
-    scanf("%s", &ptr->branch);
+    // printf("Enter Branch of Student\n");
+    // scanf("%s", &ptr->branch);
 
-    printf("Enter batch of Student\n");
-    scanf("%d", &ptr->batch);
+    // printf("Enter batch of Student\n");
+    // scanf("%d", &ptr->batch);
 
-    printf("Roll Number: %d\n", ptr->roll_no);
-    printf("Name: %s", ptr->name);//name has another newline at end
-    printf("Branch: %s\n", ptr->branch);
-    printf("Batch: %d\n", ptr->batch);
+    // printf("Roll Number: %d\n", ptr->roll_no);
+    // printf("Name: %s", ptr->name);//name has another newline at end
+    // printf("Branch: %s\n", ptr->branch);
+    // printf("Batch: %d\n", ptr->batch);
+
+
+    struct ORG org;  
+    
+    printf("The size of structure organisation : %ld\n",  sizeof(org)); 
+    
+    org.emp.employee_id = 101;   
+    strcpy(org.emp.name, "Robert"); 
+    org.emp.salary = 400000; 
+    strcpy(org.organisation_name,  "GeeksforGeeks"); 
+    strcpy(org.org_number, "GFG123768"); 
+    
+    
+
+    printf("Organisation Name : %s\n",  org.organisation_name);   
+    printf("Organisation Number : %s\n", org.org_number);   
+    printf("Employee id : %d\n", org.emp.employee_id);   
+    printf("Employee name : %s\n", org.emp.name);   
+    printf("Employee Salary : %d\n", org.emp.salary);   
 
     return 0;
 }
+
+struct Employee 
+{ 
+  int employee_id; 
+  char name[20]; 
+  int salary; 
+}; 
+ 
+typedef struct Organisation  
+{ 
+  char organisation_name[20]; 
+  char org_number[20];  
+  struct Employee emp;  
+} ORG; 
---
+++ suggestionsBySir/mergeSort.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+
+int main() {
+    int n1, n2;
+    printf("Enter the size of the array1: ");
+    scanf("%d", &n1);
+    printf("Enter the size of the array2: ");
+    scanf("%d", &n2);
+    
+    int arr1[n1];
+    int arr2[n2];
+    
+    printf("Enter the elements of the array 1\n");
+    for(int i=0; i<n1; i++){
+        scanf("%d", &arr1[i]);
+    }
+    printf("Enter the elements of the array 2\n");
+    for(int i=0; i<n2; i++){
+        scanf("%d", &arr2[i]);
+    }
+    
+    //sort both the arrays
+    
+    for(int i=0; i<n1; i++){
+        for(int j=0; j<n1-1-i; j++){
+            if(arr1[j]>arr1[j+1]){
+                int temp = arr1[j];
+                arr1[j] = arr1[j+1];
+                arr1[j+1] = temp;
+            }
+        }
+    }
+    
+    printf("Array1 after sorting:\n");
+    for(int i=0; i<n1; i++){
+        printf("%d ", arr1[i]);
+    }
+    
+    for(int i=0; i<n2; i++){
+        for(int j=0; j<n2-1-i; j++){
+            if(arr2[j]>arr2[j+1]){
+                int temp = arr2[j];
+                arr2[j] = arr2[j+1];
+                arr2[j+1] = temp;
+            }
+        }
+    }
+    
+    printf("Array2 after sorting:\n");
+    for(int i=0; i<n2; i++){
+        printf("%d ", arr2[i]);
+    }
+    
+    int c=0, c1=0, c2=0;
+    int merged[n1+n2];
+    while(c1<n1 && c2<n2){
+        if(arr1[c1]<arr2[c2]){
+            merged[c] = arr1[c1];
+            c++;
+            c1++;
+        }else if(arr1[c1]>arr2[c2]){
+            merged[c] = arr2[c2];
+            c++;
+            c2++;
+        }else{
+            merged[c] = arr1[c1];
+            c++;
+            c1++;
+        }
+    }
+
+    //if any elements are left to be added
+    while(c1<n1){
+        merged[c] = arr1[c1];
+        c1++;
+        c++;
+    }
+
+    while(c2<n2){
+        merged[c] = arr2[c2];
+        c++;
+        c2++;
+    }
+    
+    printf("Merged array\n");
+    for(int i=0; i<n1+n2; i++){
+        printf("%d ", merged[i]);
+    }
+
+    return 0;
+}
