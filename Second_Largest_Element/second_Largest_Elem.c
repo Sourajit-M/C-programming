47f51b4c0679eb529a625079618d56436b68ca3b
Sourajit-M <140721643+Sourajit-M@users.noreply.github.com>
2024-02-11 10:02:29 +0000

    second largest element in single pass

--- Second_Largest_Element/second_Largest_Elem.c
+++ Second_Largest_Element/second_Largest_Elem.c
@@ -7,22 +7,33 @@ int main(){
 
     int max = -1;
     int len = sizeof(arr)/sizeof(arr[0]);
+    int sec_Large = -1;
+
+    // for(int i=0; i<len; i++){
+    //     if(arr[i]>max){
+    //         max = arr[i];
+    //     }
+    // }
+
+    // for(int i=0; i<len; i++){
+    //     if(sec_Large<arr[i] && arr[i]<max){
+    //         sec_Large = arr[i];
+    //     }
+    // }
+
+    
 
     for(int i=0; i<len; i++){
         if(arr[i]>max){
+            sec_Large = max;
             max = arr[i];
-        }
-    }
-
-    int sec_Large = -1;
-
-    for(int i=0; i<len; i++){
-        if(sec_Large<arr[i] && arr[i]<max){
+        }else if(arr[i]>sec_Large && sec_Large<max){
             sec_Large = arr[i];
         }
     }
 
     printf("Second Largest Element in a array is : %d ", sec_Large);
+
 
     return 0;
 }
