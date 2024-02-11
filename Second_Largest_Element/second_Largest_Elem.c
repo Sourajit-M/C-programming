8d60ae043fa7ca444f934e248d20d263338aebc7
Sourajit-M <140721643+Sourajit-M@users.noreply.github.com>
2024-02-11 09:54:35 +0000

    second largest element using 2 pass

---
+++ Second_Largest_Element/second_Largest_Elem.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(){
+
+    int arr[] = {4, 6, 2, 5, 7, 9, 1, 3};
+
+    int max = -1;
+    int len = sizeof(arr)/sizeof(arr[0]);
+
+    for(int i=0; i<len; i++){
+        if(arr[i]>max){
+            max = arr[i];
+        }
+    }
+
+    int sec_Large = -1;
+
+    for(int i=0; i<len; i++){
+        if(sec_Large<arr[i] && arr[i]<max){
+            sec_Large = arr[i];
+        }
+    }
+
+    printf("Second Largest Element in a array is : %d ", sec_Large);
+
+    return 0;
+}
