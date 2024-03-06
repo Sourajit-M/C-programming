dab59ec401527487d605f06ebc590b6ade5c712a
Sourajit-M <140721643+Sourajit-M@users.noreply.github.com>
2024-03-06 14:35:24 +0000

    Stack in C

--- Data Structures/LinkedList.c
+++ Data Structures/LinkedList.c
@@ -1,41 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 typedef struct Node {
     int data;
     struct Node *next;
 } Node;
 
-
-
-Node *create_node(int data) {
-    Node *new_node = (Node *)malloc(sizeof(Node));
-    if (new_node == NULL) {
-        printf("Memory allocation failed.\n");
-        return 1;
+Node *createNode(int data) {
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        printf("Memory Allocation failed! \n");
+        exit(1);
     }
-    new_node->data = data;
-    new_node->next = NULL;
-    return new_node;
+
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
 }
 
-
-void add(Node **head, int data) {
-    Node *new_node = create_node(data);
+void add_element(Node **head, int data) {
+    Node *newNode = createNode(data);
     if (*head == NULL) {
-        *head = new_node;
+        *head = newNode;
     } else {
         Node *temp = *head;
         while (temp->next != NULL) {
             temp = temp->next;
         }
-        temp->next = new_node;
+        temp->next = newNode;
     }
 }
 
-
-void remove(Node **head, int data) {
+void remove_element(Node **head, int data) {
     Node *current = *head;
     Node *prev = NULL;
 
@@ -45,7 +41,7 @@ void remove(Node **head, int data) {
     }
 
     if (current == NULL) {
-        printf("Element not found in the list.\n");
+        printf("Element not found in the list\n");
         return;
     }
 
@@ -58,7 +54,6 @@ void remove(Node **head, int data) {
     free(current);
 }
 
-
 void display(Node *head) {
     while (head != NULL) {
         printf("%d ", head->data);
@@ -70,13 +65,13 @@ void display(Node *head) {
 int main() {
     Node *head = NULL;
 
-    add(&head, 10);
-    add(&head, 20);
-    add(&head, 30);
-    display(head);  
+    add_element(&head, 10);
+    add_element(&head, 20);
+    add_element(&head, 30);
+    display(head);
 
-    remove(&head, 20);
-    display(head);  
+    remove_element(&head, 20);
+    display(head);
 
     return 0;
 }
--- Data Structures/List.c
+++ Data Structures/List.c
@@ -1,66 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_SIZE 100
-
-
-typedef struct {
-    int elements[MAX_SIZE];
+typedef struct LinearList{
+    int elements[100];
     int size;
-} LinearList;
+} List;
 
 
-void initialize(LinearList *list) {
-    list->size = 0;
+void initialize(List *l) {
+    l->size = 0;
 }
 
 
-void add(LinearList *list, int element) {
-    if (list->size < MAX_SIZE) {
-        list->elements[list->size++] = element;
+void add(List *l, int element) {
+    if (l->size < 100) {
+        l->elements[l->size++] = element;
     } else {
         printf("List is full. Cannot add element.\n");
     }
 }
 
+//int *ptr
 
-void remove(LinearList *list, int element) {
+
+void remove_elements(List *l, int element) {
     int i, found = 0;
-    for (i = 0; i < list->size; i++) {
-        if (list->elements[i] == element) {
+    for (i = 0; i < l->size; i++) {
+        if (l->elements[i] == element) {
             found = 1;
             break;
         }
     }
     if (found) {
-        for (; i < list->size - 1; i++) {
-            list->elements[i] = list->elements[i + 1];
+        for (; i < l->size - 1; i++) {
+            l->elements[i] = l->elements[i + 1];
         }
-        list->size--;
+        l->size--;
     } else {
-        printf("Element not found in the list.\n");
+        printf("Element not found in the l.\n");
     }
 }
 
-void display(LinearList *list) {
+void display(List *l) {
     int i;
-    for (i = 0; i < list->size; i++) {
-        printf("%d ", list->elements[i]);
+    for (i = 0; i < l->size; i++) {
+        printf("%d ", l->elements[i]);
     }
     printf("\n");
 }
 
 int main() {
-    LinearList list;
-    initialize(&list);
+    List l;
+    initialize(&l);
 
-    add(&list, 10);
-    add(&list, 20);
-    add(&list, 30);
-    display(&list);
+    add(&l, 69);
+    add(&l, 70);
+    add(&l, 8);
+    display(&l);
 
-    remove(&list, 20);
-    display(&list);
+    remove_elements(&l, 8);
+    display(&l);
 
     return 0;
 }
